53d11c900e502f9b0db3f7011ddfcde1181b5acd
fouad18-meet <fouadjabr21@gmail.com>
2018-02-02 00:22:05 +0200

    database

--- hello.py
+++ hello.py
@@ -1,38 +1,80 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap 
-
+from flask import Flask, render_template, request , redirect, url_for, session
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+app.secret_key="this is my project"
 
-'''@app.route('/')
-def hello_world():
-    return '<h1>Hello, Fouad!</h1>'    '''
+from flask_sqlalchemy import SQLAlchemy
+ 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/website.db'
+ 
+db = SQLAlchemy(app)
+ 
+class Users(db.Model):
+    __tablename__="users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column('name', db.Unicode)
+    email=db.Column('email', db.Unicode)
+    comment = db.Column('comment', db.Unicode)
+ 
+ 
+db.create_all()
+
+
+
+
+
+#YOUR WEB APP CODE GOES HERE
+
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def main():
+	return render_template('index.html')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-'''
-@app.route('/')
-def index():
-    return 'Index Page'
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    if(request.method == 'GET'):
+        return render_template('contact.html')
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        comment = request.form['comment']
+        newUser = Users(name=name, email=email, comment=comment)
+        db.session.add(newUser)
+        db.session.commit()
+	return render_template('contact.html')
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
 
-@app.route('/')
-def index():
-    return 'Index Page'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-'''
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/restaurants')
+def restaurants():
+    return render_template('restaurants.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+
+
+
